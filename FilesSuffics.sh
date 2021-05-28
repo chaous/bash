e40e6dfb743205ab21537a8dcb2a59bdae5c843e
chaous <64496742+chaous@users.noreply.github.com>
2021-05-28 22:16:30 +0300

    добавил файл
    добавил файл

---
+++ FilesSuffics.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+
+python -c "
+import os
+
+path = '//Users//aleksejrozkov//Downloads'
+
+
+#print(os.listdir(path))
+#for j in path[-1::-1]:
+ #   print(j)
+# я использовал мак на windows надо // поменять на \\
+
+def get_distinct_suff(path, suff_set = set()):
+    file_list = os.listdir(path)
+    if file_list != []:
+        for i in file_list:
+            if os.path.isfile(path + '//' + i):
+                file_suff = ''
+                for j in i[-1::-1]:
+                    if j == '.':
+                        suff_set.add(file_suff[-1::-1])
+                        break
+                    file_suff += j
+            if os.path.isdir(path + '//' + i):
+                dir_set = get_distinct_suff(path + '//' + i, suff_set)
+                if type(dir_set) is set:
+                    suff_set.update(dir_set)
+        return suff_set
+
+distinct_names = get_distinct_suff(path)
+print(distinct_names)
+print(len(distinct_names))"
